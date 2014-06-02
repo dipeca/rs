a7db1c7adda2844f893545e4f9598f7e7ae5a9ff
dipeca <dipeca@gmail.com>
2014-06-02 13:23:45 +0100

    Prototype test

---
+++ Prototype/src/com/dipeca/prototype/PagQuizzAnswer.java
@@ -0,0 +1,28 @@
+package com.dipeca.prototype;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class PagQuizzAnswer extends Fragment {
+
+	RelativeLayout layout = null;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		layout = new RelativeLayout(this.getActivity());
+		layout.addView(new Button(getActivity()));
+		
+		return layout;
+	}
+
+
+
+	
+}
